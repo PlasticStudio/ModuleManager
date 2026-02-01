0237473d977ee55f632c4634558dfb3ec1d4df9c
ebakernz <emma@psdigital.co.nz>
2026-02-02 10:00:59 +1300

    migration module

---
+++ src/Legacy/Module.php
@@ -0,0 +1,10 @@
+<?php
+// app/src/Legacy/JaedbModule.php
+
+/**
+ * This is a temporary file while DB records with Jaedb\ModuleManager\Module classname exist
+ * 
+ */
+namespace Jaedb\ModuleManager;
+
+class Module extends \PlasticStudio\ModuleManager\Module {}
